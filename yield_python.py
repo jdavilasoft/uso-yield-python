67c84db5738a7130ffeec3710435a42d9cf28f43
Javier Dt <jdavila.soft@gmail.com>
2022-11-10 08:41:34 -0500

    Create yield_python.py

---
+++ yield_python.py
@@ -0,0 +1,14 @@
+def genera_pares(limite):
+    num = 1
+    while num<limite:
+        yield num*2
+        num+=1
+    
+lista_pares = genera_pares(10)
+
+print(next(lista_pares))
+print(next(lista_pares))
+
+# La funcion solo es llamada cuando se usa next
+# e itera una sola, en este caso solo genera un
+# solo numero par.
